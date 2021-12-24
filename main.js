fc0d9d4d73753571ce3a3681459e67d3bf683bc2
Catherine-prog-byte <77283675+Catherine-prog-byte@users.noreply.github.com>
2021-12-24 19:36:08 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,17 +1,38 @@
 img="";
+status_obj="";
 function setup() {
     canvas= createCanvas(640,420);
     canvas.center();
+    objectDetector=ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML ="Status : Detecting Objects";
 }
 function preload() {
-    img=loadImage("table.jpg");
+    img=loadImage("dog_cat.jpg");
 }
 function draw() {
    image(img,0,0,640,420);
    fill("#FFB600");
-   text("table",100,100);
+   text("dog", 50 , 90);
    noFill();
    textSize(25);
    stroke("#FFB600");
-   rect(80,80,500,400);
+   rect(30,60,400,350);
+
+   fill("#FFB600");
+   text("cat", 320 , 120);
+   noFill();
+   textSize(25);
+   stroke("#FFB600");
+   rect(300,90,270,320);
+}
+function modelLoaded() {
+    console.log("Model Loaded");
+    status_obj=true;
+    objectDetector.detect(img,gotResult);
+}
+function gotResult(error,results) {
+    if (error) {
+        console.log(error);
+    }
+    console.log(results);
 }
