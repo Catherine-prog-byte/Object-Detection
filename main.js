a6a81464c489d11f7a3d6bc95e29a351e3788bd4
Catherine-prog-byte <77283675+Catherine-prog-byte@users.noreply.github.com>
2021-12-24 19:26:13 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -4,14 +4,14 @@ function setup() {
     canvas.center();
 }
 function preload() {
-    img=loadImage("dog_cat.jpg");
+    img=loadImage("table.jpg");
 }
 function draw() {
    image(img,0,0,640,420);
    fill("#FFB600");
-   text("dog",50,90);
+   text("table",100,100);
    noFill();
    textSize(25);
    stroke("#FFB600");
-   rect(30,60,400,350);
+   rect(80,80,500,400);
 }
